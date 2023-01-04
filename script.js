70ed154a00590a5449d740329c6810c30ef1f456
Mateus Faustino <matheusfaustinoe20@gmail.com>
2023-01-03 21:11:46 -0300

    name, logo, layout and code changes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const newNoteBtn = document.querySelector(".create-note")
+"use strict"
 
-const modalOverlay = document.querySelector(".initial")
-const modal = document.querySelector(".initial .modal")
+let newNoteBtn = document.querySelector(".create-note")
 
-const notesContainer = document.querySelector(".my-notes")
+let modalOverlay = document.querySelector(".initial")
+let modal = document.querySelector(".initial .modal")
+
+let notesContainer = document.querySelector(".my-notes")
 
 newNoteBtn.addEventListener("click", () => {
 	modalOverlay.classList.add("active")
@@ -20,15 +22,15 @@ newNoteBtn.addEventListener("click", () => {
 document.querySelector(".modal-content").addEventListener("submit", (event) => {
 	event.preventDefault()
 
-	const title = event.submitter.parentElement[0].value
-	const content = event.submitter.parentElement[1].value
+	let title = event.submitter.parentElement[0].value
+	let content = event.submitter.parentElement[1].value
 
 	if (title.length === 0 && content.length === 0) {
 		modalOverlay.classList.remove("active")
 		modal.classList.remove("active")
 		document.querySelector("body").style.overflow = "initial"
 	} else {
-		const noteClone = document.querySelector(".for-cloning").cloneNode(true)
+		let noteClone = document.querySelector(".for-cloning").cloneNode(true)
 
 		noteClone.firstElementChild.textContent = title
 		noteClone.lastElementChild.textContent = content
@@ -45,13 +47,13 @@ document.querySelector(".modal-content").addEventListener("submit", (event) => {
 	}
 })
 
-const modalOverlayEdit = document.querySelector(".edit")
-const modalEdit = document.querySelector(".edit > .modal")
-const note = [...document.querySelectorAll(".my-notes, .notes")]
+let modalOverlayEdit = document.querySelector(".edit")
+let modalEdit = document.querySelector(".edit > .modal")
+let note = [...document.querySelectorAll(".my-notes, .notes")]
 
 note.forEach((note) => {
 	note.addEventListener("click", (event) => {
-		const mainElement = event.target
+		let mainElement = event.target
 
 		modalOverlayEdit.classList.add("active")
 		modalEdit.classList.add("active")
@@ -65,23 +67,23 @@ note.forEach((note) => {
 				document.querySelector("body").style.overflow = "initial"
 			})
 
-		const inputTitle = document.querySelector(".edit-form input")
-		const textAreaContent = document.querySelector(".edit-form textarea")
+		let inputTitle = document.querySelector(".edit-form > input")
+		let textAreaContent = document.querySelector(".edit-form > textarea")
 
-		const h3Value = event.target.firstChild.textContent
-		const paragraphValue = event.target.lastChild.textContent
+		let h3Value = event.target.firstChild.textContent
+		let paragraphValue = event.target.lastChild.textContent
 
 		inputTitle.value = h3Value
 		textAreaContent.value = paragraphValue
 
-		const h3Element = event.target.firstChild
-		const pElement = event.target.lastChild
+		let h3Element = event.target.firstChild
+		let pElement = event.target.lastChild
 
 		document.querySelector(".edit-form").addEventListener("submit", (event) => {
 			event.preventDefault()
 
-			const title = event.target[0].value
-			const content = event.target[1].value
+			let title = event.target[0].value
+			let content = event.target[1].value
 
 			if (title.length === 0 && content.length === 0) {
 				modalOverlayEdit.classList.remove("active")
@@ -97,7 +99,7 @@ note.forEach((note) => {
 			}
 		})
 
-		document.querySelector(".delete-btn").addEventListener("click", (event) => {
+		document.querySelector(".delete-btn").addEventListener("click", () => {
 			mainElement.remove()
 
 			modalOverlayEdit.classList.remove("active")
